a78f879186dedd0bd73902ee4b91b0353d57aeda
vincent-onebrief <vincent@onebrief.com>
2023-12-04 07:27:39 +0900

    Update first migration

--- src/db/postgres/migrations/001.do.sql
+++ src/db/postgres/migrations/001.do.sql
@@ -1,16 +1,37 @@
 CREATE TABLE guilds (
-    id varchar(64) PRIMARY KEY
+    "id" varchar(64) PRIMARY KEY
 );
 
 CREATE TABLE users (
-    id varchar(64) PRIMARY KEY
+    "id" varchar(64) PRIMARY KEY
 );
 
 CREATE TABLE books (
-    id integer PRIMARY KEY,
-    name varchar(128),
-    link varchar(256),
-    guild_id varchar(64),
+    "id" integer PRIMARY KEY,
+    "name" varchar(128),
+    "link" varchar(256),
+    "guild_id" varchar(64),
 
-    CONSTRAINT guild_fk FOREIGN KEY (guild_id) REFERENCES guilds (id)
+    CONSTRAINT guild_fk FOREIGN KEY ("guild_id") REFERENCES guilds ("id")
+);
+
+CREATE TABLE sections (
+    "id" integer PRIMARY KEY,
+    "description" varchar(256),
+    "order" integer,
+    "book_id" integer,
+
+    CONSTRAINT book_fk FOREIGN KEY ("book_id") REFERENCES books ("id"),
+    CONSTRAINT unique_book_section UNIQUE ("book_id", "order")
+);
+
+CREATE TABLE progresses (
+    "id" integer PRIMARY KEY,
+    "user_id" varchar(64),
+    "section_id" integer,
+    "updated" date,
+
+    CONSTRAINT user_fk FOREIGN KEY ("user_id") REFERENCES users ("id"),
+    CONSTRAINT section_fk FOREIGN KEY ("section_id") REFERENCES sections ("id"),
+    CONSTRAINT unique_user_section UNIQUE ("user_id", "section_id")
 );
